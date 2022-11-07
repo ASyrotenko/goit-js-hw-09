4dc7a6737bcbaa66cca573fd8d14235064cb4a1e
ASyrotenko <andreysyrotenko@gmail.com>
2022-11-07 21:54:14 +0200

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyBackgroundColor = {
+  intervalId: null,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    refs.startBtn.setAttribute('disabled', '');
+    this.intervalId = setInterval(() => {
+      console.log('start');
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    refs.startBtn.removeAttribute('disabled', '');
+    console.log('stop');
+  },
+};
+
+refs.startBtn.addEventListener(
+  'click',
+  bodyBackgroundColor.start.bind(bodyBackgroundColor)
+);
+refs.stopBtn.addEventListener(
+  'click',
+  bodyBackgroundColor.stop.bind(bodyBackgroundColor)
+);
